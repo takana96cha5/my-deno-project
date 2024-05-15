b2e28e124c4696b0d7cb47a0472fb0a97e393688
takana96cha5 <54287826+takana96cha5@users.noreply.github.com>
2024-05-16 07:53:32 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,10 @@
-   #!/bin/sh
+#!/bin/sh
 
-   # 引数からプロジェクト名を取得
-   PROJECT_NAME=$1
+PROJECT_NAME=$1
+WORKSPACE_URL="https://github.com/takana96cha5/my-deno-project.git"
 
-   # 雛形リポジトリのURL
-   TEMPLATE_REPO_URL="https://github.com/takana96cha5/my-deno-project.git"
-
-   # 雛形リポジトリをクローン
-   git clone ${TEMPLATE_REPO_URL} ${PROJECT_NAME}
-
-#    # .gitディレクトリを削除し、新しいGitリポジトリを初期化
-   cd ./${PROJECT_NAME} && rm -rf .git && git init
-
-#    # 不要になったinit.shを削除
-   cd .. && rm ./init.sh
-
-   echo "Project ${PROJECT_NAME} initialized successfully!"
+git clone ${WORKSPACE_URL} ${PROJECT_NAME} \
+    && cd ${PROJECT_NAME} \
+    && rm -rf .git \
+    && ./make_env.sh \
+    && rm ./init.sh
